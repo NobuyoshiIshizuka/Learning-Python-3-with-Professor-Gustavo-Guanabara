c6292fc8f5c71a58587ab53cb08bdeff6ad90068
Nobuyoshi Ishizuka <nobuyoshi_japa@hotmail.com>
2020-11-28 10:16:09 -0300

    desafio soma valores

---
+++ desafio_003.py
@@ -0,0 +1,32 @@
+"""
+Crie um program que leia dois numeros e mostre a soma entre eles.
+"""
+
+
+class Soma:
+
+    def __init__(self, num1, num2):
+        self.num1 = num1
+        self.num2 = num2
+
+    def soma_valores(self):
+        return self.num1 + self.num2
+
+    def show_type(self):
+        return type(self.num1)
+
+
+if __name__ == '__main__':
+    resultado = Soma(int(input("Entre com o primeiro número: ")),
+                     int(input("Entre com o segundo Valor: "))).soma_valores()
+
+    print(f'Os números digitados são do tipo {type(resultado)} ')
+    
+
+
+
+    print(resultado)
+
+
+
+
